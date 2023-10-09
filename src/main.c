bd55382cdaf29c490b8b7b976026182af52220d4
feivaoco <covafeiv@gmail.com>
2023-10-09 09:41:54 -0700

    sigue sin servir xdd

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@ typedef struct Nodo_t{
     struct Nodo_t *sig;
     float x;
     float y;
-    uint8_t dir;
-    uint8_t last_dir;
+    float last_x;
+    float last_y;
 }Nodo_t;
 
 void setup_snake(Nodo_t *cabeza);
@@ -23,7 +23,7 @@ float comida_y;
 float comida_timer;
 uint8_t se_comio;
 uint32_t score;
-
+uint8_t dir;
 int main(void){
     
     const int screenWidth = 800;
@@ -56,7 +56,7 @@ void setup_snake(Nodo_t *cabeza){
     score = 0;
     cabeza->x = (float)GetScreenWidth()/4;
     cabeza->y = (float)GetScreenHeight()/4;
-    cabeza->dir = 0;
+    dir = 0;
     vel = 4.0;
     se_comio = 0;
     comida_x = GetRandomValue(0 + 10, GetScreenWidth() - 10);
@@ -65,89 +65,64 @@ void setup_snake(Nodo_t *cabeza){
 }
 
 void update_snake(Nodo_t *cabeza){
-    cabeza->last_dir = cabeza->dir;
-    if(IsKeyDown(KEY_D)) cabeza->dir = cabeza->dir == 1 ? 1:0;
-    if(IsKeyDown(KEY_A)) cabeza->dir = cabeza->dir == 0 ? 0:1;
-    if(IsKeyDown(KEY_W)) cabeza->dir = cabeza->dir == 3 ? 3:2;
-    if(IsKeyDown(KEY_S)) cabeza->dir = cabeza->dir == 2 ? 2:3;
-    switch(cabeza->dir){
+    
+    if(IsKeyDown(KEY_D)) dir = dir == 1 ? 1:0;
+    if(IsKeyDown(KEY_A)) dir = dir == 0 ? 0:1;
+    if(IsKeyDown(KEY_W)) dir = dir == 3 ? 3:2;
+    if(IsKeyDown(KEY_S)) dir = dir == 2 ? 2:3;
+    cabeza->last_x = cabeza->x;
+    cabeza->last_y = cabeza->y;
+    if(cabeza->sig != NULL){
+        cabeza->sig->x = cabeza->last_x;
+        cabeza->sig->y = cabeza->last_y;
+    }
+    switch(dir){
         case 0:
-            cabeza->dir = 0;
+            cabeza->x += vel;
             break;
         case 1:
-            cabeza->dir = 1;
+            cabeza->x -= vel;
             break;
         case 2:
-            cabeza->dir = 2;
+            cabeza->y -= vel;
             break;
         case 3:
-            cabeza->dir = 3;
+            cabeza->y += vel;
             break;
     }
+    if (cabeza->y > GetScreenHeight()+10) cabeza->y = 0;
+    if (cabeza->y < -10) cabeza->y = GetScreenHeight();
+    if (cabeza->x > GetScreenWidth()+10) cabeza->x = 0;
+    if (cabeza->x < -10) cabeza->x = GetScreenWidth();
     uint8_t si_sig = 0;
     Nodo_t *nc = cabeza;
-    while(1){
-        
-        if (nc->y > GetScreenHeight()+10) nc->y = 0;
-        if (nc->y < -10) nc->y = GetScreenHeight();
-        if (nc->x > GetScreenWidth()+10) nc->x = 0;
-        if (nc->x < -10) nc->x = GetScreenWidth();
-        switch (nc->dir) {
-            case 0:
-                nc->x += vel;
-                break;
-            case 1:
-                nc->x -= vel;
-                break;
-            case 2:
-                nc->y -= vel;
-                break;
-            case 3:
-                nc->y += vel;
-                break; 
-        }
-        if(nc->sig != NULL){ 
-            nc->sig->last_dir = nc->sig->dir; 
-            nc->sig->dir = nc->last_dir;    
-            nc = nc->sig;
-        }else {
-            break;
-        }
+    while(nc->sig !=NULL){
+        nc = nc->sig;
+         
+    
         
     }
-    
+
     if(CheckCollisionRecs((Rectangle){cabeza->x-10,cabeza->y-10,20,20}, (Rectangle){comida_x-4,comida_y-4,8,8})){
         comida_x = GetRandomValue(0 + 10, GetScreenWidth() - 10);
         comida_y = GetRandomValue(0 + 10, GetScreenHeight() - 10);
         score += 1;
         vel *= 1.01;
-        Nodo_t *nd = cabeza;
+        nc = cabeza;
         uint8_t se_aniadio_nodo = 0; 
         while(!se_aniadio_nodo){
-            if(nd->sig == NULL){
-                nd->sig = malloc(sizeof(Nodo_t));
-                switch (nc->dir) {
-                    case 0:
-                        nc->sig->dir = 0;nc->sig->x = nc->x - 10;nc->sig->y = nc->y;
-                        break;
-                    case 1:
-                        nc->sig->dir = 1;nc->sig->x = nc->x + 10;nc->sig->y = nc->y;
-                        break;
-                    case 2:
-                        nc->sig->dir = 2;nc->sig->x = nc->x;nc->sig->y = nc->y + 10;
-                        break;
-                    case 3:
-                        nc->sig->dir = 3;nc->sig->x = nc->x;nc->sig->y = nc->y - 10;
-                        break;
-                }
-                nd->sig->last_dir = nd->sig->dir;
-                nd->sig->sig = NULL;
+            if(nc->sig == NULL){
+                nc->sig = malloc(sizeof(Nodo_t));
+                nc->sig->y = nc->last_y;
+                nc->sig->x = nc->last_x;
+                nc->sig->sig = NULL;
                 se_aniadio_nodo = 1;     
             }else{
-                nd = nd->sig;
+                nc = nc->sig;
             }
         }        
     }
+        
     
     
 }
