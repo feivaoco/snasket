9928a597b49ae471dd3f0ee790d174a2cca90c74
feivaoco <covafeiv@gmail.com>
2023-10-09 14:14:49 -0700

    modificacion al algoritmo de aniadir nodos

--- src/main.c
+++ src/main.c
@@ -4,10 +4,12 @@
 #include <stdlib.h>
 typedef struct Nodo_t{
     struct Nodo_t *sig;
+    struct Nodo_t *ant;
     float x;
     float y;
     float last_x;
     float last_y;
+    
 }Nodo_t;
 
 void setup_snake(Nodo_t *cabeza);
@@ -22,8 +24,9 @@ float comida_x;
 float comida_y;
 float comida_timer;
 uint8_t se_comio;
-uint32_t score;
 uint8_t dir;
+int nodos_aniadir;
+uint32_t nodos_cantidad;
 int main(void){
     
     const int screenWidth = 800;
@@ -32,7 +35,10 @@ int main(void){
     SetTargetFPS(60);
     
     Nodo_t* cabeza = malloc(sizeof(Nodo_t));
+    Nodo_t* cola   = malloc(sizeof(Nodo_t));
     cabeza->sig = NULL;
+    
+    
     
     setup_snake(cabeza);
     
@@ -53,14 +59,16 @@ int main(void){
 }
 
 void setup_snake(Nodo_t *cabeza){
-    score = 0;
+    nodos_aniadir = 0;
     cabeza->x = (float)GetScreenWidth()/4;
     cabeza->y = (float)GetScreenHeight()/4;
     dir = 0;
-    vel = 4.0;
+    vel = 6.0;
     se_comio = 0;
+    nodos_cantidad = 0;
     comida_x = GetRandomValue(0 + 10, GetScreenWidth() - 10);
     comida_y = GetRandomValue(0 + 10, GetScreenHeight() - 10);   
+    
 }
 
 void update_snake(Nodo_t *cabeza){
@@ -104,18 +112,28 @@ void update_snake(Nodo_t *cabeza){
             nc->sig->x = nc->last_x;
             nc->sig->y = nc->last_y;     
         }
-         
-    
         
     }
+    if(nodos_aniadir > 0){
+        
+        nc->sig = malloc(sizeof(Nodo_t));
+        nc->sig->y = nc->last_y; 
+        nc->sig->x = nc->last_x; 
+        nc->sig->sig = NULL;
+    
+        nodos_aniadir -= 1;
+        if(nodos_aniadir < 0) nodos_aniadir = 0;
+    }
+    
+    
 
     if(CheckCollisionRecs((Rectangle){cabeza->x-10,cabeza->y-10,20,20}, (Rectangle){comida_x-4,comida_y-4,8,8})){
         comida_x = GetRandomValue(0 + 10, GetScreenWidth() - 10);
         comida_y = GetRandomValue(0 + 10, GetScreenHeight() - 10);
-        score += 1;
-        vel *= 1.01;
-        nc = cabeza;
-        uint8_t se_aniadio_nodo = 0; 
+        nodos_aniadir += 5;
+        nodos_cantidad += 5;
+        //vel *= 1.01;
+        /*uint8_t se_aniadio_nodo = 0; 
         while(!se_aniadio_nodo){
             if(nc->sig == NULL){
                 nc->sig = malloc(sizeof(Nodo_t));
@@ -126,7 +144,7 @@ void update_snake(Nodo_t *cabeza){
             }else{
                 nc = nc->sig;
             }
-        }        
+        } */       
     }
         
     
@@ -135,8 +153,7 @@ void update_snake(Nodo_t *cabeza){
 
 
 void draw_snake(Nodo_t *cabeza){
-    DrawText(TextFormat("%f",vel), GetScreenWidth() - 120, GetScreenHeight() - 50, 50, SKYBLUE);
-    DrawText(TextFormat("%i",score), 20, GetScreenHeight() - 50, 50, SKYBLUE);
+    DrawText(TextFormat("%i",nodos_cantidad), 20, GetScreenHeight() - 50, 50, SKYBLUE);
     //DrawRectangleRec((Rectangle){cabeza.x-10,cabeza.y-10,20,20}, GREEN);
     DrawCircle(cabeza->x,cabeza->y, 10, BLUE);
     Nodo_t *nc = cabeza;
