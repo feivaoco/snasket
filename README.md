a2dec68ded473ba7e86a9c33db95ff386812cf55
feivaoco <covafeiv@gmail.com>
2023-10-09 20:40:36 -0700

    se aniadio el readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# SNASKET
+### Titulo inspirado en "Caskets" del NehruvianDOOM
+
+#### raylib
+<https://github.com/raysan5/raylib>
+#### libpd
+<https://github.com/libpd/libpd>
+#### portaudio
+<https://github.com/PortAudio/portaudio>
+
+
+#### El compilador utilizado de gcc es del [w64devkit](https://github.com/skeeto/w64devkit).
+#### Al igual que el GNU Make es parte de ese kit.
+
+Creo que no nada mas que decir. El proyecto es una prueba de 
+porfin hacer un jueguito en raylib e integrar Pure Data mediante 
+la libreria de libpd. 
+
+Si no te sirve la compilacion aun usando w64devkit entonces debes
+realizar lo siguiente:
+1. Descargar el codigo fuente de libpd y seguir sus instrucciones de compilacion
+2. Colocar los archivos resultado de la compilacion en sus respectivas carpetas en este proyecto
+3. Descargar el codigo fuente de portaudio y compilarlo
+4. Colocar los archivos resultado de la compilacion en sus respectivas carpetas en este proyecto   
--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@ typedef struct Nodo_t{
     float y;
     float last_x;
     float last_y;
-    
 }Nodo_t;
 
 void setup_snake(Nodo_t *cabeza);
@@ -23,9 +22,8 @@ float comida_x;
 float comida_y;
 float comida_timer;
 uint8_t se_comio;
+uint32_t score;
 uint8_t dir;
-int nodos_aniadir;
-uint32_t nodos_cantidad;
 int main(void){
     
     const int screenWidth = 800;
@@ -35,8 +33,6 @@ int main(void){
     
     Nodo_t* cabeza = malloc(sizeof(Nodo_t));
     cabeza->sig = NULL;
-    
-    
     
     setup_snake(cabeza);
     
@@ -57,41 +53,21 @@ int main(void){
 }
 
 void setup_snake(Nodo_t *cabeza){
-    nodos_aniadir = 0;
+    score = 0;
     cabeza->x = (float)GetScreenWidth()/4;
     cabeza->y = (float)GetScreenHeight()/4;
     dir = 0;
-    vel = 6.0;
+    vel = 4.0;
     se_comio = 0;
-    nodos_cantidad = 0;
     comida_x = GetRandomValue(0 + 10, GetScreenWidth() - 10);
     comida_y = GetRandomValue(0 + 10, GetScreenHeight() - 10);   
-    if(cabeza->sig != NULL){
-        Nodo_t *tmp = cabeza->sig;
-        Nodo_t *tmp_borrar;
-        cabeza->sig = NULL;
-        while(1){
-            tmp_borrar = tmp;
-            if(tmp->sig == NULL){
-                tmp = NULL;
-                free(tmp_borrar);
-                break;
-            }else {
-                tmp = tmp->sig;
-                free(tmp_borrar);
-            }
-        }
-
-        
-    } 
 }
 
 void update_snake(Nodo_t *cabeza){
-    if(IsKeyDown(KEY_I)) setup_snake(cabeza);
-    if(IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) dir = dir == 1 ? 1:0;
-    if(IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) dir = dir == 0 ? 0:1;
-    if(IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) dir = dir == 3 ? 3:2;
-    if(IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) dir = dir == 2 ? 2:3;
+    if(IsKeyDown(KEY_D)) dir = dir == 1 ? 1:0;
+    if(IsKeyDown(KEY_A)) dir = dir == 0 ? 0:1;
+    if(IsKeyDown(KEY_W)) dir = dir == 3 ? 3:2;
+    if(IsKeyDown(KEY_S)) dir = dir == 2 ? 2:3;
     cabeza->last_x = cabeza->x;
     cabeza->last_y = cabeza->y;
     if(cabeza->sig != NULL){
@@ -128,28 +104,15 @@ void update_snake(Nodo_t *cabeza){
             nc->sig->x = nc->last_x;
             nc->sig->y = nc->last_y;     
         }
-        
     }
-    if(nodos_aniadir > 0){
-        
-        nc->sig = malloc(sizeof(Nodo_t));
-        nc->sig->y = nc->last_y; 
-        nc->sig->x = nc->last_x; 
-        nc->sig->sig = NULL;
-    
-        nodos_aniadir -= 1;
-        if(nodos_aniadir < 0) nodos_aniadir = 0;
-    }
-    
-    
-
-    if(CheckCollisionRecs((Rectangle){cabeza->x-10,cabeza->y-10,20,20}, (Rectangle){comida_x-4,comida_y-4,8,8})){
+    Rectangle rec_cabeza = {cabeza->x-10,cabeza->y-10,20,20};
+    if(CheckCollisionRecs(rec_cabeza, (Rectangle){comida_x-4,comida_y-4,8,8})){
         comida_x = GetRandomValue(0 + 10, GetScreenWidth() - 10);
         comida_y = GetRandomValue(0 + 10, GetScreenHeight() - 10);
-        nodos_aniadir += 5;
-        nodos_cantidad += 5;
-        //vel *= 1.01;
-        /*uint8_t se_aniadio_nodo = 0; 
+        score += 1;
+        vel *= 1.01;
+        nc = cabeza;
+        uint8_t se_aniadio_nodo = 0; 
         while(!se_aniadio_nodo){
             if(nc->sig == NULL){
                 nc->sig = malloc(sizeof(Nodo_t));
@@ -160,16 +123,14 @@ void update_snake(Nodo_t *cabeza){
             }else{
                 nc = nc->sig;
             }
-        } */       
+        }        
     }
-        
-    
-    
 }
 
 
 void draw_snake(Nodo_t *cabeza){
-    DrawText(TextFormat("%i",nodos_cantidad), 20, GetScreenHeight() - 50, 50, SKYBLUE);
+    DrawText(TextFormat("%f",vel), GetScreenWidth() - 120, GetScreenHeight() - 50, 50, SKYBLUE);
+    DrawText(TextFormat("%i",score), 20, GetScreenHeight() - 50, 50, SKYBLUE);
     //DrawRectangleRec((Rectangle){cabeza.x-10,cabeza.y-10,20,20}, GREEN);
     DrawCircle(cabeza->x,cabeza->y, 10, BLUE);
     Nodo_t *nc = cabeza;
